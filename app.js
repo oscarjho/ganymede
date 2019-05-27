373c8dde044476ca61e38d934171974b6e8c1aff
Oscar <oscarjavierho@gmail.com>
2019-05-27 08:19:48 -0430

    Develop change

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
-// @route   GET /
+/// @route   GET /
 // @desc    /
 // @access  Public
 app.get('/', (req, res) => res.json({ msg: 'ganymede' }));
